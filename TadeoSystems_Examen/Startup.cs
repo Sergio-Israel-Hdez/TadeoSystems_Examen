0d49ceaa3a1a1b6aa4c3f4ceb8a097970d84620f
SergioHF <sergio.israel.hernandez@gmail.com>
2020-12-18 14:51:06 -0600

    cors agregados

--- TadeoSystems_Examen/Startup.cs
+++ TadeoSystems_Examen/Startup.cs
@@ -25,6 +25,7 @@ namespace TadeoSystems_Examen
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
             services.AddControllers();
         }
 
@@ -39,6 +40,8 @@ namespace TadeoSystems_Examen
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("AllowAll");
 
             app.UseAuthorization();
 
